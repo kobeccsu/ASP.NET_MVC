9d8038aa8db3904d6600ac9fd73e641d191c27b1
Lei Zhou <Lei_Zhou@epam.com>
2018-05-25 17:44:21 +0800

    check in

---
+++ DemoMVC/Controllers/AjaxCenterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class AjaxCenterController : Controller
+    {
+        // GET: AjaxCenter
+        public JsonResult Index()
+        {
+            var json = "{many:1}";
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: AjaxCenter/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("Home/good")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult RedirectView()
+        {
+            Thread.Sleep(3000);
+            TempData["Name"] = "Lei zhou";
+            return RedirectToAction("TempView");
+        }
+
+        public ActionResult TempView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify()
+        {
+            var username = Request.Form["username"];
+            
+            if (username == "zhoulei") {
+                TempData["Name"] = username;
+                return RedirectToAction("TempView");
+            }
+            return View("Index");
+        }
+    }
+}
